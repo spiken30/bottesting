906520d8b8263d89339528bd9e0a3dcd2fc5f19c
Javier Torres <javier.torres@icomsys.com.mx>
2018-02-21 13:39:35 -0600

    [UPDATED] - index updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Webhook server is listening, port 3000'));
 
 app.get('/', function (req, res) {
     res.status(200).send("Got it dude!");
