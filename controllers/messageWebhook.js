7d25f8932b9a2fc4f65e7704ac49dc9c1576e3d2
Javier Torres <javier.torres@icomsys.com.mx>
2018-02-21 15:19:22 -0600

    [UPDATED]

--- controllers/messageWebhook.js
+++ controllers/messageWebhook.js
@@ -1,6 +1,7 @@
 //const processMessage = require('../helpers/processMessage');
 
 module.exports = (req, res) => {
+  console.log(req);
   if (req.body.object === 'page') {
     req.body.entry.forEach(entry => {
       entry.messaging.forEach(event => {
