e2f96318ee8d61f071600f2e29ddeb07d334420b
Javier Torres <javier.torres@icomsys.com.mx>
2018-02-21 13:58:38 -0600

    [UPDATED]

--- controllers/verification.js
+++ controllers/verification.js
@@ -1,12 +1,17 @@
 module.exports = (req, res) => {
- const hubChallenge = req.query['hub.challenge'];
+  try {
+    const hubChallenge = req.query['hub.challenge'];
 
-const hubMode = req.query['hub.mode'];
- const verifyTokenMatches = (req.query['hub.verify_token'] === 'Bottesting');
+   const hubMode = req.query['hub.mode'];
+    const verifyTokenMatches = (req.query['hub.verify_token'] === 'Bottesting');
 
-if (hubMode && verifyTokenMatches) {
- res.status(200).send(hubChallenge);
- } else {
- res.status(403).end();
- }
+   if (hubMode && verifyTokenMatches) {
+    res.status(200).send(hubChallenge);
+    } else {
+    res.status(403).end();
+    }
+  } catch(Exception ex)
+  {
+    console.log(ex);
+  }
 };
