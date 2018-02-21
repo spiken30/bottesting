cb41e8e039c7d8d93a26ae9486a6199a16ba18cf
Javier Torres <javier.torres@icomsys.com.mx>
2018-02-21 13:55:09 -0600

    [UPDATED] - index updated

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(process.env.PORT || 3000, () => console.log('Webhook server is listening, port 3000'));
-const verificationController = require(‘./controllers/verification’);
-const messageWebhookController = require(‘./controllers/messageWebhook’);
+const verificationController = require('./controllers/verification');
+const messageWebhookController = require('./controllers/messageWebhook');
 
-app.get(‘/’, verificationController);
-app.post(‘/’, messageWebhookController);
+app.get('/', verificationController);
+app.post('/', messageWebhookController);
